0a48c7ced7e0b7d530ceda1f96c653c197193e1a
Albar Moerhamsa <moerhamsa@gmail.com>
2024-01-21 13:42:37 +0700

    feat: create module cart and cart item

--- app.js
+++ app.js
@@ -1,34 +1,36 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
+const express = require("express");
 const App = express();
-const cors = require('cors');
+const cors = require("cors");
 const PORT = process.env.PORT || 8080;
-const logger = require('morgan');
-const routes = require('./src/routes');
-const errorHandler = require('./src/middleware/errorHandler');
+const logger = require("morgan");
+const routes = require("./src/routes");
+const errorHandler = require("./src/middleware/errorHandler");
 
-App.use(logger('dev'));
+App.use(logger("dev"));
 App.use(cors());
 
-App.use(express.urlencoded({ extended: 'false' }));
+App.use(express.urlencoded({ extended: "false" }));
 App.use(express.json());
 
 App.use(routes);
 App.use(errorHandler);
 
-App.get('/', (req, res) => {
-  return res.status(200).json({ status_message: `BAKO_ABAH_SRV is running on port ${PORT}` });
+App.get("/", (req, res) => {
+    return res
+        .status(200)
+        .json({ status_message: `BAKO_ABAH_SRV is running on port ${PORT}` });
 });
 
 // Define the static file path
-App.use(express.static(__dirname + '/public/uploads'));
-App.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
+App.use(express.static(__dirname + "/public/uploads"));
+App.get("/", function (req, res) {
+    res.sendFile(__dirname + "/index.html");
 });
 
 App.listen(PORT, () => {
-  console.log(`This Server running on port ${PORT}`);
+    console.log(`This Server running on port ${PORT}`);
 });
 
 module.exports = App;
